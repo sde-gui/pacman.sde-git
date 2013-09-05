3f45c0888007405ab58faf4dbfefe9db5f553a25
Vadim Ushakov <igeekless@gmail.com>
2013-09-06 04:52:24 +0800

    add build.sh

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+set -x
+
+build()
+{
+    (cd $1 && rm -f *.xz && makepkg && yaourt -U *.xz)
+}
+
+build libsmfm-core-git
+build libsmfm-gtk2-git
+build libsde-utils-git
+build libsde-utils-jansson-git
+build stuurman-git
+build stuurman-desktop-git
+build waterline-git
