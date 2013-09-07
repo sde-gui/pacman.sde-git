87b6a19ac0706fd78be71db01f285b1975804df9
Vadim Ushakov <igeekless@gmail.com>
2013-09-07 19:22:34 +0800

    add libsde-utils-gtk2-git

--- build.sh
+++ build.sh
@@ -11,6 +11,7 @@ build()
 build libsmfm-core-git
 build libsmfm-gtk2-git
 build libsde-utils-git
+build libsde-utils-gtk2-git
 build libsde-utils-jansson-git
 build stuurman-git
 build stuurman-desktop-git
