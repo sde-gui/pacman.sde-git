c80b822d4d1b89ad3d9a7be36a9e7aa2a2a4dcfb
Vadim Ushakov <igeekless@gmail.com>
2013-09-07 22:41:20 +0800

    reorder items in build.sh

--- build.sh
+++ build.sh
@@ -8,11 +8,11 @@ build()
     (cd $1 && rm -f *.xz && makepkg && yaourt -U *.xz)
 }
 
-build libsmfm-core-git
-build libsmfm-gtk2-git
 build libsde-utils-git
 build libsde-utils-gtk2-git
 build libsde-utils-jansson-git
+build libsmfm-core-git
+build libsmfm-gtk2-git
 build stuurman-git
 build stuurman-desktop-git
 build waterline-git
