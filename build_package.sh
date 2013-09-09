c5a7a8729e0bcb04a6adad0cd8f5a1d7056377ec
Vadim Ushakov <igeekless@gmail.com>
2013-09-09 19:57:03 +0800

    append hour and minute to package version

--- build_package.sh
+++ build_package.sh
@@ -4,4 +4,4 @@ set -e
 set -x
 
 mkdir -p repo
-cd ./$1 && rm -f *.xz && makepkg && sudo yaourt --noconfirm -U `readlink -f *.xz` && cp *.xz ../repo/ && touch ../repo/.timestamp.build.$1
+cd ./$1 && rm -fr *.xz src/ && makepkg && sudo yaourt --noconfirm -U `readlink -f *.xz` && cp *.xz ../repo/ && touch ../repo/.timestamp.build.$1
