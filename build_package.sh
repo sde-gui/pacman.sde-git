d5d73aad9d38023a523e1062f4e1b11aaebe6194
Vadim Ushakov <igeekless@gmail.com>
2015-07-01 22:01:34 +0700

    install packages with pacman, not with yaourt

--- build_package.sh
+++ build_package.sh
@@ -3,5 +3,18 @@
 set -e
 set -x
 
+pacman_queue() {
+    local lockfile=/var/lib/pacman/db.lck
+    if [[ -f ${lockfile} ]]; then
+        echo 'Pacman is currently in use, waiting...'
+        while [[ -f ${lockfile} ]]; do sleep 5; done
+    fi
+}
+
+_pacman() {
+    pacman_queue
+    sudo pacman "$@"
+}
+
 mkdir -p repo
-cd ./$1 && rm -fr *.xz src/ && makepkg && sudo yaourt --noconfirm -U `readlink -f *.xz` && cp *.xz ../repo/ && touch ../repo/.timestamp.build.$1
+cd ./$1 && rm -fr *.xz src/ && makepkg && _pacman --noconfirm -U `readlink -f *.xz` && cp *.xz ../repo/ && touch ../repo/.timestamp.build.$1
