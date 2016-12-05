b34478629b4ff607605fc0f5654eb03b6b8f1bd5
Vadim Ushakov <igeekless@gmail.com>
2016-12-05 10:10:42 +0700

    Fix parsing of multiline arrays in gen_deps.sh

--- gen_deps.sh
+++ gen_deps.sh
@@ -10,8 +10,8 @@ printf "PACKAGES = %s\n" "`echo $PACKAGES`"
 for p in $PACKAGES ; do
     unset -v depends
     unset -v makedepends
-    eval "`egrep '^depends=\(.*\)' "$p/PKGBUILD"`"
-    eval "`egrep '^makedepends=\(.*\)' "$p/PKGBUILD"`"
+    eval "`grep -Ezo '[[:space:]]depends=\([^)]*\)' "$p/PKGBUILD"`"
+    eval "`grep -Ezo '[[:space:]]makedepends=\([^)]*\)' "$p/PKGBUILD"`"
     for dep in "${depends[@]}" "${makedepends[@]}" ; do
         for p1 in $PACKAGES ; do
             if [ "$dep" = "$p1" ] ; then
@@ -20,3 +20,4 @@ for p in $PACKAGES ; do
         done
     done
 done
+
