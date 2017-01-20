e7d1162cdeee66998b18bfa0e48bb7b6b7ea9e15
Vadim Ushakov <igeekless@gmail.com>
2017-01-20 19:22:00 +0700

    delete aur_upload.sh

--- aur_upload.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-set -x
-
-find . -maxdepth 2 -name 'PKGBUILD' -printf '%h\n' | while read dir ; do
-    (cd $dir && rm -f *.src.tar.gz && makepkg --source && burp -c x11 *.src.tar.gz)
-done
